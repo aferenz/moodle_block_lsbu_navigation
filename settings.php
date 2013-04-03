14e6a9ca0a9d8e2c2fd813879c30dd04aef7ee6d
ian.wild <ian.wild@heavy-horse.co.uk>
2013-04-03 09:12:32 +0100

    Improvement: make display of 'Site home', 'Site pages' and 'My profile' links optional.

--- settings.php
+++ settings.php
@@ -17,4 +17,13 @@ if ($ADMIN->fulltree) {
     
     $dbtype = new admin_setting_configselect('block_lsbu_navigation/displaytype', get_string('displaytype','block_lsbu_navigation'), '', '', $options);
     $settings->add($dbtype);
+    
+    $dbdebug = new admin_setting_configcheckbox('block_lsbu_navigation/showsitehome', get_string( 'showsitehome', 'block_lsbu_navigation'), '', 1);
+    $settings->add($dbdebug);
+    
+    $dbdebug = new admin_setting_configcheckbox('block_lsbu_navigation/showsitepages', get_string( 'showsitepages', 'block_lsbu_navigation'), '', 1);
+    $settings->add($dbdebug);
+    
+    $dbdebug = new admin_setting_configcheckbox('block_lsbu_navigation/showmyprofile', get_string( 'showmyprofile', 'block_lsbu_navigation'), '', 1);
+    $settings->add($dbdebug);
 }
